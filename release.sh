e326b3e3c3222f0291804374d8f680e24e2419be
Hakan Ardo <hakan.ardo@math.lth.se>
2024-11-06 14:52:28 +0100

    release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh
+
+set  -e
+
+if [ ! -z "`git diff`" ]; then
+    echo Please commit first
+    exit
+fi
+
+git tag -a v$(python -c 'import setup; print(setup.__version__)')  -m release
+git push --tags
+
--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ import os
 with open(os.path.join(os.path.dirname(__file__), "sskit", "version.py")) as fp:
     exec(fp.read())
 
-setup(
-    name='sskit',
-    description='Spiideo Scenes development Kit',
-    long_description='''
-        Development Kit for the Spiideo SoccerNet SynLoc task and Challange
-    ''',
-    version=__version__,
-    packages=['sskit'],
-    zip_safe=False,
-    url='https://github.com/Spiideo/sskit',
-    author='Hakan Ardo',
-    author_email='hakan.ardo@spiideo.com',
-    license='MIT',
-)
+if __name__ == '__main__':
+    setup(
+        name='sskit',
+        description='Spiideo Scenes development Kit',
+        long_description='''
+            Development Kit for the Spiideo SoccerNet SynLoc task and Challange
+        ''',
+        version=__version__,
+        packages=['sskit'],
+        zip_safe=False,
+        url='https://github.com/Spiideo/sskit',
+        author='Hakan Ardo',
+        author_email='hakan.ardo@spiideo.com',
+        license='MIT',
+    )
